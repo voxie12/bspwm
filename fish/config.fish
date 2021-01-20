b300e189bb347c06ec8565d34302d07aed4508ac
Jay <65469239+voxie12@users.noreply.github.com>
2021-01-20 12:17:21 +0000

    update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,34 @@
-set PATH $HOME/.config/rofi/bin $PATH
-alias insta="sudo dnf install -y"
-alias update="sudo dnf update -y"
-alias autoremove="sudo dnf autoremove -y"
-alias remove="sudo dnf remove -y"
-alias search="sudo dnf search"
-alias upgrade="sudo dnf upgrade-minimal -y"
-alias ls="exa -lah"
-alias v="vim"
+#! /bin/bash
 
+# first run when u open terminal
+colorscript -e 31
+
+# alias
+alias y='yay -Syu --noconfirm'
+alias remove='yay -Rds'
+alias autoremove='yay -Ycc'
+alias clean='yay -Scc'
+alias cl='clear'
+alias v='vim'
+alias ls='exa -la --group-directories-first'
+alias config.fish='vim $HOME/.config/fish/config.fish'
+alias vbspwm='vim $HOME/.config/bspwm/bspwmrc'
+alias vkitty='vim $HOME/.config/kitty/kitty.conf'
+alias x='startx'
+alias df='df -h'
+alias free='free -h'
+alias cp='cp -i'
+alias rm='rm -i'
+alias mv='mv -i'
+alias nf='neofetch'
+alias nfoff='neofetch --off'
+alias bat='acpi'
+alias fdisk='sudo fdisk -l'
+alias plaza='mpv https://radio.plaza.one/mp3'
+alias youtube-best='youtube-dl -f bestvideo+bestaudio '
+alias youtube-mp3='youtube-dl --extract-audio --audio-format mp3 '
+alias poweroff='sudo powroff'
+alias reboot='sudo reboot'
+
+# prompt
 starship init fish | source
