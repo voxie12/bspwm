426182658b2f5c9fabb8baf1881568263bdb4032
Jay <65469239+voxie12@users.noreply.github.com>
2021-02-24 15:30:47 +0000

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,17 +4,18 @@
 colorscript -e 31
 
 # alias
-alias y='yay -Syu --noconfirm'
-alias remove='yay -Rds'
-alias autoremove='yay -Ycc'
-alias clean='yay -Scc'
+alias y='paru -Syu --noconfirm'
+alias remove='paru -Rds'
+alias autoremove='paru -c'
+alias clean='paru -Scc'
 alias cl='clear'
 alias v='vim'
-alias ls='exa -la --group-directories-first --icons'
+alias ls='exa -al --color=always --group-directories-first'
 alias vfish='vim $HOME/.config/fish/config.fish'
 alias vbspwm='vim $HOME/.config/bspwm/bspwmrc'
 alias vsxhkd='vim $HOME/.config/sxhkd/sxhkdrc'
 alias vkitty='vim $HOME/.config/kitty/kitty.conf'
+alias vbar='$HOME/.config/polybar/config'
 alias x='startx'
 alias df='df -h'
 alias free='free -h'
@@ -23,7 +24,7 @@ alias rm='rm -i'
 alias mv='mv -i'
 alias nf='neofetch'
 alias nfoff='neofetch --off'
-alias bat='acpi'
+alias battery='acpi'
 alias fdisk='sudo fdisk -l'
 alias plaza='mpv https://radio.plaza.one/mp3'
 alias youtube-best='youtube-dl -f bestvideo+bestaudio '
@@ -32,7 +33,6 @@ alias poweroff='sudo powroff'
 alias reboot='sudo reboot'
 alias r='ranger'
 alias n='nnn'
-
 
 # prompt
 starship init fish | source
